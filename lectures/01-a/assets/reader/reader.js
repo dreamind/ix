09d8921f74e80223fdb138b75d88848e8f530156
ivo <ivow@unimelb.edu.au>
2015-02-06 15:55:36 +1100

    Update mind reader

--- lectures/01-a/assets/reader/reader.js
+++ lectures/01-a/assets/reader/reader.js
@@ -1,134 +1,133 @@
 
-
-var cards;
-var candidates;
-
-  function addStyles(cssRules) {
-      $('head').append('<style>' + cssRules + '</style>');
-  }
-
-
+function addStyles(cssRules) {
+  $('head').append('<style>' + cssRules + '</style>');
+}
 
 function generateAllCardStyles() {
-    var style = '', row, col;
-    for (var i = 0; i < 52; i++) {
-        row = i / 13 >> 0;
-        col = i % 13;
-        style += _.sprintf('.card-%d { background-position: -%dpx -%dpx; }',
-          i, 79*col, 123*row);
-    }
-    return style;
+  var style = '', row, col;
+  for (var i = 0; i < 52; i++) {
+    row = i / 13 >> 0;
+    col = i % 13;
+    style += _.sprintf('.card-%d { background-position: -%dpx -%dpx; }\n',
+      i, 79 * col, 123 * row);
+  }
+  return style;
 }
-
 
 function generateCardStyles() {
-    var style = '';
-    for (var row = 0; row < 7; row++) {
-        for (var col = 0; col < 3; col++) {
-         style += _.sprintf('#card-row%d-col%d { left: -%dpx; right: -%dpx; }',
-           row, col, (col * 102) + Math.floor(Math.random() * 3) * 4, row * 60);
-        }
+  var style = '';
+  for (var row = 0; row < 3; row++) {
+    for (var col = 0; col < 7; col++) {
+      style += _.sprintf('#card-row%d-col%d { left: %dpx; top: %dpx; }\n',
+        row, col, col * 60, row * 150 + Math.floor(Math.random() * 15));
     }
-    return style;
+  }
+  return style;
 }
 
-  function buildCards(cards) {
-    var k;
-   for (var row = 0; row < 7; row++) {
-    for (var col = 0; col < 3; col++) {
-     k = col * 7 + row;
-     card = '<div class="card card-%d" id="card-row%d-col%d"></div>' % (cards[k], row, col)
-     ';
-     $('#cards').append(card);
+function buildCards(cards) {
+  var card, cardsEl = $('#cards'), k;
+  cardsEl.empty();
+  for (var row = 0; row < 3; row++) {
+    for (var col = 0; col < 7; col++) {
+      k = col + row * 7;
+      card = _.sprintf('<div class="card card-%d" id="card-row%d-col%d"></div>',
+        cards[k], row, col);
+      cardsEl.append(card);
     }
-   }
-
-  /*
-
-
-
-def produce_content( card_idxs, selected ):
-    content = {}
-    content["styles"] = gen_style1()+gen_style2()
-    content["hiddens"] = gen_hiddens(card_idxs, selected)
-    if len(selected) == 1:
-        content["cards"] = '<div class="card card%d" id="r3_c1"></div>' % selected[0]
-        content["action"] = '<a href="magic.2.py">Replay</a>'
-        content["styles"] += '''<style>
-#col_selection{
-    display:none;
+  }
+  $('#step-button').val('Next');
+  $('input[name=row]').prop('checked', false);
+  $('#column-selection').show();
 }
-</style>'''
-    else:
-        content["cards"] = gen_cards( card_idxs )
-        content["action"] = '<input type="submit" value="Read My Card">'
-    return content
 
-def produce_html( content ):
-    template = init_template( "template.2.html" )
-    html = expand_template( template, content )
-    print 'Content-Type: text/html\n'
-    print html
+function resolveCard(candidate) {
+  var card, cardsEl = $('#cards'), row = 1, col = 3;
+  cardsEl.empty();
+  card = _.sprintf('<div class="card card-%d" id="card-row%d-col%d"></div>',
+    candidate, row, col);
+  cardsEl.append(card);
+  $('#step-button').val('Replay');
+  $('#column-selection').hide();
+}
 
-def split_n( a_list, n ):
-    results = [ list([]) for i in range(n) ]
-    i = 0
-    for an_item in a_list:
-        results[i].append( an_item )
-        i += 1
-        if i == n:
-            i = 0
-    return results
+function splitCardsTrio(cards) {
+  var parts = [[], [], []], i = 0;
+  _.each(cards, function (card) {
+    parts[i].push(card);
+    i = (i+1) % 3;
+  });
+  return parts;
+}
 
-def init_cards():
-    all_cards = range(0,52) # 0-51
-    random.shuffle( all_cards )
-    card_idxs = all_cards[:21] # take the first 21
-    return card_idxs
+function initCards() {
+  var cards = _.range(52);
+  cards = _.shuffle(cards)
+  return cards.slice(0, 21);
+}
 
-def get_user_input():
-    form = cgi.FieldStorage()
-    if "column" in form: #user has given some input
-        column = int(form.getfirst("column"))
-        card_idxs = [ int(i)  for i in form.getlist("card_idxs") ]
-        prev_probable_cards = [ int(i)  for i in form.getlist("probable_cards") ]
-    else:
-        column = -1
-        card_idxs = init_cards()
-        prev_probable_cards = []
-    return column, card_idxs, prev_probable_cards
+function getCandidates(cards, candidates, k) {
+  var a = _.intersection(
+    cards.slice(k * 7, (k + 1) * 7),
+    candidates
+  );
+  return a;
+}
 
+function obfuscateCards(cards, candidates) {
+  var others = _.difference(cards, candidates);
+  others = _.shuffle(others);
+  candidates = _.shuffle(candidates);
+  var parts = splitCardsTrio(candidates);
+  cards = [];
+  for (var i = 0; i < 3; i++) {
+    var row = parts[i];
+    var padLength = 7 - row.length;
+    var pad = others.splice(0, padLength);
+    row = _.shuffle(row.concat(pad));
+    cards = cards.concat(row);
+  }
+  return cards;
+}
 
-function obfuscate_cards( cards,  ):
-   ignored_cards = list( set( card_idxs ) - set( probable_cards ) )
-   random.shuffle( ignored_cards )
-   random.shuffle( probable_cards )
-   cards_3cols = split_n( probable_cards, 3 )
-   card_idxs = []
+function main() {
 
-def obfuscate_cards( card_idxs, probable_cards ):
-    ignored_cards = list( set( card_idxs ) - set( probable_cards ) )
-    random.shuffle( ignored_cards )
-    random.shuffle( probable_cards )
-    cards_3cols = split_n( probable_cards, 3 )
-    card_idxs = []
-    for i in range(3):
-        paddings = 7 - len( cards_3cols[i] ) # padding from ignored cards
-        cards_3cols[i].extend( [ ignored_cards.pop() for j in range(paddings) ] )
-        random.shuffle( cards_3cols[i] )
-        card_idxs.extend( cards_3cols[i] )
-    return card_idxs, probable_cards
+  var cards, candidates, k;
+  addStyles(generateAllCardStyles() + generateCardStyles());
 
-def main():
-    column, card_idxs, prev_probable_cards = get_user_input()
-    if column == -1: # first step
-        probable_cards = list(card_idxs)
-    else: # subsequent steps
-        current_probable_cards = set( card_idxs[column*7:(column+1)*7] )
-        prev_probable_cards = set( prev_probable_cards )
-        probable_cards = list( current_probable_cards & prev_probable_cards )
-    card_idxs, probable_cards = obfuscate_cards( card_idxs, probable_cards )
-    content = produce_content( card_idxs, probable_cards )
-    produce_html( content )
+  function init() {
+    cards = initCards();
+    candidates = cards.slice();
+  }
 
-main()*/
+  function step() {
+    cards = obfuscateCards(cards, candidates);
+    buildCards(cards);
+  }
+
+  init();
+  step();
+  $('#step-button').click(function (ev) {
+    if (ev.target.value === 'Replay') {
+      init();
+      step();
+    } else {
+      k = parseInt($('input[name=row]:checked').val());
+      if (isNaN(k)) {
+        alert('Please select a row!');
+        return;
+      }
+      candidates = getCandidates(cards, candidates, k);
+      if (candidates.length === 1) {
+        resolveCard(candidates[0]);
+      } else {
+        step();
+      }
+    }
+  });
+}
+
+_.mixin(s.exports());
+$(document).ready(function () {
+  main();
+});
