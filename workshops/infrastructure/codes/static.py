5c9906135cb32183751c22aa881a90dc021172ac
ivo <ivow@unimelb.edu.au>
2016-03-11 16:41:49 +1100

    Update week-3 workshop and lecture

--- workshops/infrastructure/codes/static.py
+++ workshops/infrastructure/codes/static.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__, static_folder='.', static_url_path='')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='127.0.0.1', port=80)
 
 
