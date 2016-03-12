023541cc4cd141604034c528d2b15deb7b1c637e
ivo <ivow@unimelb.edu.au>
2016-03-13 00:45:43 +1100

    Update samples

--- lectures/i18n/codes/locale/demo.py
+++ lectures/i18n/codes/locale/demo.py
@@ -1,4 +1,5 @@
-#!/path-to-your/python.exe
+from flask import Flask
+app = Flask(__name__, static_folder='.', static_url_path='')
 '''
 Run only in Windows machine
 
@@ -9,35 +10,40 @@ import sys
 import time
 import cgitb; cgitb.enable()
 
-print "Content-Type: text/html\n"
-print '''<html>
-<head><meta http-equiv="content-type" content="text/html; charset=UTF-8" /></head>
-<body>
-'''
-# the list of locale tuples
-# each tupple contains a title, locale name, and the encoding it uses
-sample_locales = [
-	('France', 'French_France.1252', 'cp1252'),
-	('USA', 'English_US.1252', 'cp1252'),
-	('Australia', 'English_Australia.1252', 'cp1252'),
-	('German', 'German_Germany.1252', 'cp1252' ),
-	('Chinese Taiwan', 'Chinese_Taiwan.950', 'cp950' ),
-	('Chinese PRC', "Chinese_People's Republic of China.936", 'cp936' ),
-	('Japanese', 'Japanese_Japan.932', 'cp932' )
-]
+def gen_html():
+	str = '''<html>
+	<head><meta http-equiv="content-type" content="text/html; charset=UTF-8" /></head>
+	<body>
+	'''
+	# the list of locale tuples
+	# each tupple contains a title, locale name, and the encoding it uses
+	sample_locales = [
+		('France', 'French_France.1252', 'cp1252'),
+		('USA', 'English_US.1252', 'cp1252'),
+		('Australia', 'English_Australia.1252', 'cp1252'),
+		('German', 'German_Germany.1252', 'cp1252' ),
+		('Chinese Taiwan', 'Chinese_Taiwan.950', 'cp950' ),
+		('Chinese PRC', "Chinese_People's Republic of China.936", 'cp936' ),
+		('Japanese', 'Japanese_Japan.932', 'cp932' )
+	]
 
-for title, loc, enc in sample_locales:
-	locale_str = locale.setlocale(locale.LC_ALL, loc)
+	for title, loc, enc in sample_locales:
+		locale_str = locale.setlocale(locale.LC_ALL, loc)
 
-	currency_string = locale.currency(1234.56) # byte string
-	date_string = time.strftime('%X %x %A')
+		currency_string = locale.currency(1234.56) # byte string
+		date_string = time.strftime('%X %x %A')
 
-	# print as utf-8 encoded to utf-8 console
-	print "<p>Locale set in %s: %s, %s</p>" % \
-		(title, \
-		currency_string.decode(enc).encode('utf-8'), \
-		date_string.decode(enc).encode('utf-8'))
+		# print as utf-8 encoded to utf-8 console
+		str += "<p>Locale set in %s: %s, %s</p>" % \
+			(title, \
+			currency_string.decode(enc).encode('utf-8'), \
+			date_string.decode(enc).encode('utf-8'))
 
-print '''</body>
-</html>
-'''
+	str += '''</body></html>'''
+
+@app.route("/demo-locale")
+def root():
+    return gen_html()
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- lectures/i18n/codes/multilangs/multilangs.py
+++ lectures/i18n/codes/multilangs/multilangs.py
@@ -1,13 +1,15 @@
-#!/Users/dreamind/anaconda/bin/python
+from flask import Flask, request
+import os
+
+app = Flask(__name__, static_folder='.', static_url_path='')
+path = os.path.abspath(os.path.dirname(__file__)) + '/'
 from lxml import etree
-import cgitb;cgitb.enable()
-import cgi
 import locale
 import time
 
 # reading configuration file, languages.xml
 # to find out supported languges and their information
-tree = etree.parse('languages-nix.xml')
+tree = etree.parse(path + 'languages-nix.xml')
 root = tree.getroot()
 langs = {}
 current_lang = 'English' # use this a global, not a good practice
@@ -23,7 +25,7 @@ for e_lang in root:
 def lang_setup():
   global current_lang
   lang = langs[current_lang]
-  lang_tree = etree.parse(lang['trans-src'])
+  lang_tree = etree.parse(path + lang['trans-src'])
   lang_def = lang_tree.getroot()
   trans = {}
   body = lang_def.find('file').find("body")
@@ -36,74 +38,82 @@ def lang_setup():
 
 # get formatted date
 def get_date():
-  return time.strftime('%A, %x ').decode(langs[current_lang]['locale-enc']).encode('utf-8')
+  return time.strftime('%A, %x ').decode(langs[current_lang]['locale-enc'])
 
 # get formatted time
 def get_time():
-  return time.strftime('%X').decode(langs[current_lang]['locale-enc']).encode('utf-8')
+  return time.strftime('%X').decode(langs[current_lang]['locale-enc'])
 
 # get a translation of a text
 def get_trans(text):
-  return langs[current_lang]['trans'][text].encode('utf-8')
+  return langs[current_lang]['trans'][text]
 
 _ = get_trans # a shorthand
 
 # display the whole HTML page
 def display_page():
   global current_lang
-  print "Content-Type: text/html\n"
-  print '''
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  str = '''
+<!DOCTYPE html">
+<html>
 <head><meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <title>Multilingual sites demo</title>
 </head>
 '''
-  print '<body>'
-  field_store = cgi.FieldStorage()
-  if 'lang_button' in field_store:
-    current_lang = field_store.getfirst('lang')
+  str += '<body>'
+  lang_btn = request.args.get('lang_button')
+  go_btn = request.args.get('go_button')
+  if lang_btn:
+    current_lang = request.args.get('lang')
     lang_setup()
-    display_form()
-  elif 'go_button' in field_store:
-    current_lang = field_store.getfirst('lang')
-    name = field_store.getfirst('name')
+    str += display_form()
+  elif go_btn:
+    current_lang = request.args.get('lang')
+    name = request.args.get('name')
     lang_setup()
-    display_response(name)
+    str += display_response(name)
   else: # first invocation of the app
     current_lang = "English" # default
     lang_setup()
-    display_form()
-  print '</body>'
-  print '</html>'
+    str += display_form()
+  str += '</body>'
+  str += '</html>'
+  return str
 
 # display the body for the page
 def display_form():
-  print '<form method="get" action="">'
-  print '<p>'
-  print '<select name="lang">'
+  str = '<form method="get" action="">'
+  str += '<p>'
+  str += '<select name="lang">'
   for lang in langs:
     if lang == current_lang:
-      print '<option value="%s" selected="selected">%s</option>' % (lang, lang)
+      str += '<option value="%s" selected="selected">%s</option>' % (lang, lang)
     else:
-      print '<option value="%s">%s</option>' % (lang, lang)
+      str += '<option value="%s">%s</option>' % (lang, lang)
 
-  print '</select>'
-  print '<input type="submit" name="lang_button" value="Select languages"/>'
-  print '</p>'
+  str += '</select>'
+  str += '<input type="submit" name="lang_button" value="Select languages"/>'
+  str += '</p>'
 
-  print '<p>'
-  print _('Please enter your name')
-  print '<input type="text" name="name" />'
-  print '<input type="submit" name="go_button" value="Go!" />'
-  print '</form>'
-  print '</p>'
+  str += '<p>'
+  str += _('Please enter your name')
+  str += '<input type="text" name="name" />'
+  str += '<input type="submit" name="go_button" value="Go!" />'
+  str += '</form>'
+  str += '</p>'
+  return str
 
 def display_response(name):
-  print '<h1>' + _('Hello,') + ' ' + name + '</h1>'
-  print '<h2>'
-  print _('Today is') + ' ' + get_date() + '.'
-  print _('The time now is') + ' ' + get_time()
-  print '</h2>'
+  str = '<h1>' + _('Hello,') + ' ' + name + '</h1>'
+  str += '<h2>'
+  str += _('Today is') + ' ' + get_date() + '.'
+  str += _('The time now is') + ' ' + get_time()
+  str += '</h2>'
+  return str
 
-display_page()
+@app.route("/demo-multilang")
+def root():
+    return display_page()
+
+if __name__ == "__main__":
+    app.run(debug=True)
